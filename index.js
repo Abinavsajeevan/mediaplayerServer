3124d71cf157a3ec088ffd25ff39e67e06cc5328
Abinavsajeevan <abinavabi2k02@gmail.com>
2024-07-16 20:59:57 +0530

    server side of mediaplayer

---
+++ index.js
@@ -0,0 +1,27 @@
+// import json server
+const jsonServer = require('json-server')
+
+// create json server - create()
+
+const mediaPlayerServer = jsonServer.create();
+
+// create path for the json file(data) -router()
+
+const router  = jsonServer.router('db.json')
+
+// create middleware to parse json
+
+const middleware = jsonServer.defaults()
+
+// allow server to use router and middleware
+mediaPlayerServer.use(middleware)
+mediaPlayerServer.use(router)
+
+//  set up port for the server to run
+PORT = 3000  || process.env.PORT 
+
+// listen to the request
+
+mediaPlayerServer.listen(PORT, ()=>{
+    console.log(`Server running successfully at port number ${PORT}`);
+})
